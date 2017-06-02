90c135e96442fa580ea93887972b4491bffe9038
sam <samuel.klein@online.de>
2017-06-02 05:04:50 +0200

    added various assets

---
+++ android/assets/map_test/uf_tileset_map.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="uf_map" tilewidth="24" tileheight="24" tilecount="525" columns="25">
+ <image source="uf_map.png" width="600" height="504"/>
+</tileset>
