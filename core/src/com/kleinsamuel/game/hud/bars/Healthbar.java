4480e63462c4ea9f4e59cfeb2f6d66a2b81c2ade
sam <samuel.klein@online.de>
2017-06-04 00:55:23 +0200

    changed size of bars

--- core/src/com/kleinsamuel/game/hud/bars/Healthbar.java
+++ core/src/com/kleinsamuel/game/hud/bars/Healthbar.java
@@ -1,12 +1,9 @@
 package com.kleinsamuel.game.hud.bars;
 
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.Vector3;
 import com.kleinsamuel.game.model.Assets;
 import com.kleinsamuel.game.screens.PlayScreen;
-import com.kleinsamuel.game.util.Utils;
 
 /**
  * Created by sam on 30.05.17.
@@ -16,37 +13,34 @@ public class Healthbar {
 
     private final int PADDING_LEFT = 10;
     private final int PADDING_TOP = 10;
-    private final int INTERNAL_PADDING = 8;
-    private final float WIDTH = PlayScreen.V_WIDTH*0.30f;
-    private final float HEIGHT = PlayScreen.V_HEIGHT*0.1f;
-    private int currentHealth, maxHealth;
 
-    public float CURRENT_WIDTH = WIDTH-(2*INTERNAL_PADDING);
+    private final float WIDTH = PlayScreen.V_WIDTH*0.30f;
+    private final float HEIGHT = PlayScreen.V_HEIGHT*0.075f;
+
+    public float CURRENT_WIDTH = WIDTH;
 
     private double computePercent(int current, int max) {
         return (current*1.0)/(max*1.0);
     }
 
     public void setHealth(int current, int max){
-        currentHealth = current;
-        maxHealth = max;
         double perc = computePercent(current, max);
-        CURRENT_WIDTH = (float)(perc*(WIDTH-(2*INTERNAL_PADDING))-20f);
+        CURRENT_WIDTH = (float)(perc*WIDTH);
     }
 
     public void render(SpriteBatch batch){
-        batch.draw(Assets.manager.get(Assets.empty_bar, Texture.class), PADDING_LEFT, PlayScreen.V_HEIGHT-HEIGHT-PADDING_TOP, WIDTH, HEIGHT);
-        batch.draw(Assets.manager.get(Assets.red_bar, Texture.class), PADDING_LEFT+INTERNAL_PADDING, PlayScreen.V_HEIGHT-HEIGHT-PADDING_TOP+INTERNAL_PADDING-2, CURRENT_WIDTH, HEIGHT-(2*(INTERNAL_PADDING-2)));
 
+        batch.draw(Assets.manager.get(Assets.uf_healthbar, Texture.class), PADDING_LEFT, PlayScreen.V_HEIGHT-HEIGHT-PADDING_TOP, WIDTH, HEIGHT);
+        batch.draw(Assets.manager.get(Assets.rectangle_black, Texture.class), PADDING_LEFT+WIDTH-(WIDTH-CURRENT_WIDTH), PlayScreen.V_HEIGHT-HEIGHT-PADDING_TOP, WIDTH-CURRENT_WIDTH, HEIGHT);
+
+        /*
         Utils.basicFont.getData().setScale(0.8f, 0.8f);
         Utils.basicFont.setColor(Color.WHITE);
-
         String s = currentHealth+"/"+maxHealth;
-
         Vector3 dims = Utils.getWidthAndHeightOfString(Utils.basicFont, s);
         float width = dims.x;
         float height = dims.y;
-        //Utils.basicFont.draw(batch, s, (WIDTH/2)-(width/2)+PADDING_LEFT, PlayScreen.V_HEIGHT-PADDING_TOP-(HEIGHT/2));
-
+        Utils.basicFont.draw(batch, s, (WIDTH/2)-(width/2)+PADDING_LEFT, PlayScreen.V_HEIGHT-PADDING_TOP-(HEIGHT/2));
+        */
     }
 }
--- core/src/com/kleinsamuel/game/hud/bars/Manabar.java
+++ core/src/com/kleinsamuel/game/hud/bars/Manabar.java
@@ -10,29 +10,32 @@ import com.kleinsamuel.game.screens.PlayScreen;
  */
 
 public class Manabar {
-    private final int PADDING_LEFT = 10;
-    private final int PADDING_TOP = 43;
-    private final int INTERNAL_PADDING = 8;
-    private final float WIDTH = PlayScreen.V_WIDTH*0.30f;
-    private final float HEIGHT = PlayScreen.V_HEIGHT*0.073f;
 
-    public float CURRENT_WIDTH = WIDTH-(2*INTERNAL_PADDING+1);
+    private final float PADDING_LEFT = 10.0f;
+    private final float PADDING_TOP = 38.0f;
+
+    private final float WIDTH = PlayScreen.V_WIDTH*0.3f;
+    private final float HEIGHT = PlayScreen.V_HEIGHT*0.06f;
+
+    public float CURRENT_WIDTH = WIDTH;
 
     private double computePercent(int current, int max) {
         return (current*1.0)/(max*1.0);
     }
 
     public void setMana(int current, int max){
+
         double perc = computePercent(current, max);
-        CURRENT_WIDTH = (int)(perc*(WIDTH-(2*(INTERNAL_PADDING+1))));
+        CURRENT_WIDTH = (int)(perc*WIDTH);
     }
 
     public void render(SpriteBatch batch){
 
-        batch.draw(Assets.manager.get(Assets.empty_bar, Texture.class), PADDING_LEFT, PlayScreen.V_HEIGHT-HEIGHT-PADDING_TOP, WIDTH, HEIGHT);
-        batch.draw(Assets.manager.get(Assets.blue_bar, Texture.class), PADDING_LEFT+INTERNAL_PADDING-1, PlayScreen.V_HEIGHT-HEIGHT-PADDING_TOP+INTERNAL_PADDING-4, CURRENT_WIDTH, HEIGHT-(2*(INTERNAL_PADDING-4)));
-        //batch.draw(Assets.manager.get(Assets.rectangle_black, Texture.class), PADDING_LEFT, PlayScreen.V_HEIGHT-HEIGHT-PADDING_TOP, WIDTH, HEIGHT);
-        //batch.draw(Assets.manager.get(Assets.rectangle_gray, Texture.class), PADDING_LEFT+INTERNAL_PADDING, PlayScreen.V_HEIGHT-HEIGHT-PADDING_TOP+INTERNAL_PADDING, WIDTH-(2*INTERNAL_PADDING), HEIGHT-(2*INTERNAL_PADDING));
-        //batch.draw(Assets.manager.get(Assets.rectangle_blue, Texture.class), PADDING_LEFT+INTERNAL_PADDING, PlayScreen.V_HEIGHT-HEIGHT-PADDING_TOP+INTERNAL_PADDING, CURRENT_WIDTH, HEIGHT-(2*INTERNAL_PADDING));
+        batch.draw(Assets.manager.get(Assets.uf_manabar, Texture.class), PADDING_LEFT, PlayScreen.V_HEIGHT-HEIGHT-PADDING_TOP, WIDTH, HEIGHT);
+        batch.draw(Assets.manager.get(Assets.rectangle_black, Texture.class), PADDING_LEFT+WIDTH-(WIDTH-CURRENT_WIDTH), PlayScreen.V_HEIGHT-HEIGHT-PADDING_TOP, WIDTH-CURRENT_WIDTH, HEIGHT);
+
+        //batch.draw(Assets.manager.get(Assets.empty_bar, Texture.class), PADDING_LEFT, PlayScreen.V_HEIGHT-HEIGHT-PADDING_TOP, WIDTH, HEIGHT);
+        //batch.draw(Assets.manager.get(Assets.blue_bar, Texture.class), PADDING_LEFT+INTERNAL_PADDING-1, PlayScreen.V_HEIGHT-HEIGHT-PADDING_TOP+INTERNAL_PADDING-4, CURRENT_WIDTH, HEIGHT-(2*(INTERNAL_PADDING-4)));
+
     }
 }
