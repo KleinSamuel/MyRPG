70956cf124ffa7d946d601849250cd9e6f5d1a7c
sam <samuel.klein@online.de>
2017-06-06 02:32:37 +0200

    added base multiplier class but this may be redundant

---
+++ core/src/com/kleinsamuel/game/model/data/stats/BaseMultiplier.java
@@ -0,0 +1,45 @@
+package com.kleinsamuel.game.model.data.stats;
+
+/**
+ * Created by sam on 06.06.17.
+ */
+
+public class BaseMultiplier {
+
+    /* multiplier for damage of attack */
+    public float MULTIPLIER_ATTACK = 1.0f;
+
+    /* multiplier for attack speed */
+    public float MULTIPLIER_ATTACK_SPEED = 1.0f;
+
+    /* multiplier for reducing taken damage */
+    public float MULTIPLIER_DEFENSE = 1.0f;
+
+    /* multiplier for MAX_HEALTH */
+    public float MULTIPLIER_HEALTH = 1.0f;
+
+    /* multiplier for MAX_MANA */
+    public float MULTIPLIER_MANA = 1.0f;
+
+    /* multiplier for CURRENT_EXPERIENCE gain */
+    public float MULTIPLIER_EXPERIENCE = 1.0f;
+
+    /* mulitplier for moving speed */
+    public float MULTIPLIER_MOVING_SPEED = 1.0f;
+
+    /* multiplier for range weapons range */
+    public float MULTIPLIER_RANGE = 1.0f;
+
+    /* multiplier for critical hit damage */
+    public float MULTIPLIER_CRIT_DAMAGE = 1.0f;
+
+    /* multiplier for critical hit chance */
+    public float MULTIPLIER_CRIT_CHANCE = 1.0f;
+
+    /* mutliplier on weapons effect */
+    public float MULTIPLIER_WEAPON_EFFECT = 1.0f;
+
+    /* multiplier on random events such as item drops or gambling */
+    public float MULTIPLIER_LUCK = 1.0f;
+
+}
