21355ba1fc917cabecdc3498732e62828b65e36d
sam <samuel.klein@online.de>
2017-06-07 01:07:23 +0200

    added click listener for switch buttons and show money

--- core/src/com/kleinsamuel/game/hud/lexicon/Lexicon.java
+++ core/src/com/kleinsamuel/game/hud/lexicon/Lexicon.java
@@ -4,7 +4,8 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.kleinsamuel.game.model.Assets;
 import com.kleinsamuel.game.screens.PlayScreen;
-import com.kleinsamuel.game.util.Utils;
+
+import static com.kleinsamuel.game.util.Utils.FACTOR;
 
 /**
  * Created by sam on 03.06.17.
@@ -17,10 +18,20 @@ public class Lexicon {
     public static boolean SHOW_LEXICON = false;
 
     /* coordinates for close button of bag */
-    public final float CLOSE_X_1 = 909.0f * Utils.FACTOR;
-    public final float CLOSE_Y_1 = 492.0f * Utils.FACTOR;
-    public final float CLOSE_X_2 = 941.0f * Utils.FACTOR;
-    public final float CLOSE_Y_2 = 523.0f * Utils.FACTOR;
+    public final float CLOSE_X_1 = 909.0f * FACTOR;
+    public final float CLOSE_Y_1 = 492.0f * FACTOR;
+    public final float CLOSE_X_2 = 941.0f * FACTOR;
+    public final float CLOSE_Y_2 = 523.0f * FACTOR;
+
+    /* coordinates for stat and bag buttons */
+    public final float BAG_X_1 = 30.0f * FACTOR;
+    public final float BAG_Y_1 = 490.0f * FACTOR;
+    public final float BAG_X_2 = 112.0f * FACTOR;
+    public final float BAG_Y_2 = 525.0f * FACTOR;
+    public final float STAT_X_1 = 157.0f * FACTOR;
+    public final float STAT_Y_1 = 490.0f * FACTOR;
+    public final float STAT_X_2 = 238.0f * FACTOR;
+    public final float STAT_Y_2 = 525.0f * FACTOR;
 
     public Lexicon(PlayScreen playScreen){
         this.playScreen = playScreen;
@@ -29,6 +40,16 @@ public class Lexicon {
     public void handleClick(int screenX, int screenY) {
         if (clickOnExit(screenX, screenY)) {
             SHOW_LEXICON = false;
+            return;
+        }
+        if(clickOnBag(screenX, screenY)){
+            SHOW_LEXICON = false;
+            playScreen.bag.SHOW_BAG = true;
+            return;
+        }
+        if(clickOnStat(screenX, screenY)){
+            SHOW_LEXICON = false;
+            playScreen.stats.SHOW_STATS = true;
             return;
         }
     }
@@ -40,9 +61,25 @@ public class Lexicon {
         return false;
     }
 
+    public boolean clickOnBag(int screenX, int screenY){
+        if(screenX >= BAG_X_1 && screenX <= BAG_X_2 && screenY >= BAG_Y_1 && screenY <= BAG_Y_2) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean clickOnStat(int screenX, int screenY){
+        if(screenX >= STAT_X_1 && screenX <= STAT_X_2 && screenY >= STAT_Y_1 && screenY <= STAT_Y_2) {
+            return true;
+        }
+        return false;
+    }
+
     public void render(SpriteBatch batch) {
 
         batch.draw(Assets.manager.get(Assets.lexicon_background, Texture.class), 0, 0, PlayScreen.V_WIDTH, PlayScreen.V_HEIGHT);
+
+        playScreen.hud.drawMoneyString(batch);
 
     }
 
--- core/src/com/kleinsamuel/game/hud/stats/Stats.java
+++ core/src/com/kleinsamuel/game/hud/stats/Stats.java
@@ -4,7 +4,8 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.kleinsamuel.game.model.Assets;
 import com.kleinsamuel.game.screens.PlayScreen;
-import com.kleinsamuel.game.util.Utils;
+
+import static com.kleinsamuel.game.util.Utils.FACTOR;
 
 /**
  * Created by sam on 03.06.17.
@@ -17,10 +18,20 @@ public class Stats {
     public static boolean SHOW_STATS = false;
 
     /* coordinates for close button of bag */
-    public final float CLOSE_X_1 = 909.0f * Utils.FACTOR;
-    public final float CLOSE_Y_1 = 492.0f * Utils.FACTOR;
-    public final float CLOSE_X_2 = 941.0f * Utils.FACTOR;
-    public final float CLOSE_Y_2 = 523.0f * Utils.FACTOR;
+    public final float CLOSE_X_1 = 909.0f * FACTOR;
+    public final float CLOSE_Y_1 = 492.0f * FACTOR;
+    public final float CLOSE_X_2 = 941.0f * FACTOR;
+    public final float CLOSE_Y_2 = 523.0f * FACTOR;
+
+    /* coordinates for bag and encyclopedia buttons */
+    public final float BAG_X_1 = 30.0f * FACTOR;
+    public final float BAG_Y_1 = 490.0f * FACTOR;
+    public final float BAG_X_2 = 112.0f * FACTOR;
+    public final float BAG_Y_2 = 525.0f * FACTOR;
+    public final float ENCY_X_1 = 159.0f * FACTOR;
+    public final float ENCY_Y_1 = 490.0f * FACTOR;
+    public final float ENCY_X_2 = 238.0f * FACTOR;
+    public final float ENCY_Y_2 = 525.0f * FACTOR;
 
     public Stats(PlayScreen playScreen){
         this.playScreen = playScreen;
@@ -29,6 +40,16 @@ public class Stats {
     public void handleClick(int screenX, int screenY) {
         if (clickOnExit(screenX, screenY)) {
             SHOW_STATS = false;
+            return;
+        }
+        if(clickOnBag(screenX, screenY)){
+            SHOW_STATS = false;
+            playScreen.bag.SHOW_BAG = true;
+            return;
+        }
+        if(clickOnLexicon(screenX, screenY)){
+            SHOW_STATS = false;
+            playScreen.bag.SHOW_BAG = true;
             return;
         }
     }
@@ -40,9 +61,25 @@ public class Stats {
         return false;
     }
 
+    public boolean clickOnBag(int screenX, int screenY) {
+        if(screenX >= BAG_X_1 && screenX <= BAG_X_2 && screenY >= BAG_Y_1 && screenY <= BAG_Y_2) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean clickOnLexicon(int screenX, int screenY) {
+        if(screenX >= ENCY_X_1 && screenX <= ENCY_X_2 && screenY >= ENCY_Y_1 && screenY <= ENCY_Y_2) {
+            return true;
+        }
+        return false;
+    }
+
     public void render(SpriteBatch batch) {
 
         batch.draw(Assets.manager.get(Assets.stats_background, Texture.class), 0, 0, PlayScreen.V_WIDTH, PlayScreen.V_HEIGHT);
+
+        playScreen.hud.drawMoneyString(batch);
 
     }
 }
