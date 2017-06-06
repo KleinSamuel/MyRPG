3d97837f28f94cd3b38c1cc3d66199831507fb15
sam <samuel.klein@online.de>
2017-06-06 02:29:27 +0200

    added enum for races

---
+++ core/src/com/kleinsamuel/game/model/data/races/RacesEnum.java
@@ -0,0 +1,17 @@
+package com.kleinsamuel.game.model.data.races;
+
+/**
+ * Created by sam on 06.06.17.
+ */
+
+public enum RacesEnum {
+
+    DWARF,
+    ELB,
+    HALFLING,
+    HUMAN,
+    MURONY, // VAMPIRE
+    ORC,
+    WIEDERGAENGER;
+
+}
