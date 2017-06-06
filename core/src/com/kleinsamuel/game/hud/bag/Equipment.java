5c14bf21596e6654a70760ce9694e9c54e857f12
sam <samuel.klein@online.de>
2017-06-07 01:07:41 +0200

    added equipment

--- core/src/com/kleinsamuel/game/hud/bag/Equipment.java
+++ core/src/com/kleinsamuel/game/hud/bag/Equipment.java
@@ -11,21 +11,17 @@ public class Equipment {
 
     public static final int HELMET_SLOT_ID = 1;
     public static final int TORSO_SLOT_ID = 2;
-    public static final int LEFT_ARM_SLOT_ID = 3;
-    public static final int RIGHT_ARM_SLOT_ID = 4;
-    public static final int LEFT_HAND_SLOT_ID = 5;
-    public static final int RIGHT_HAND_SLOT_ID = 6;
-    public static final int LEGS_SLOT_ID = 7;
-    public static final int LEFT_FOOT_SLOT_ID = 8;
-    public static final int RIGHT_FOOT_SLOT_ID = 9;
-    public static final int LEFT_RING_SLOT_ID = 10;
-    public static final int RIGHT_RING_SLOT_ID = 11;
-    public static final int NECKLACE_SLOT_ID = 12;
+    public static final int LEFT_HAND_SLOT_ID = 3;
+    public static final int RIGHT_HAND_SLOT_ID = 4;
+    public static final int LEGS_SLOT_ID = 5;
+    public static final int LEFT_FOOT_SLOT_ID = 6;
+    public static final int RIGHT_FOOT_SLOT_ID = 7;
+    public static final int LEFT_RING_SLOT_ID = 8;
+    public static final int RIGHT_RING_SLOT_ID = 9;
+    public static final int NECKLACE_SLOT_ID = 10;
 
     public static HashSet<Integer> itemsForHelmet;
     public static HashSet<Integer> itemsForTorso;
-    public static HashSet<Integer> itemsForLeftArm;
-    public static HashSet<Integer> itemsForRightArm;
     public static HashSet<Integer> itemsForLeftHand;
     public static HashSet<Integer> itemsForRightHand;
     public static HashSet<Integer> itemsForLegs;
@@ -36,16 +32,16 @@ public class Equipment {
     public static HashSet<Integer> itemsForNecklace;
 
     static {
-        itemsForHelmet = new HashSet<Integer>(Arrays.asList(4,12));
-        itemsForTorso = new HashSet<Integer>(Arrays.asList(5));
-        itemsForLeftHand = new HashSet<Integer>(Arrays.asList(7));
-        itemsForRightHand = new HashSet<Integer>(Arrays.asList(8));
-        itemsForLegs = new HashSet<Integer>(Arrays.asList(6));
-        itemsForLeftFoot = new HashSet<Integer>(Arrays.asList(9));
-        itemsForRightFoot = new HashSet<Integer>(Arrays.asList(10));
-        itemsForLeftRing = new HashSet<Integer>(Arrays.asList(-1));
-        itemsForRightRing = new HashSet<Integer>(Arrays.asList(-1));
-        itemsForNecklace = new HashSet<Integer>(Arrays.asList(11));
+        itemsForHelmet = new HashSet<Integer>(Arrays.asList(2));
+        itemsForTorso = new HashSet<Integer>(Arrays.asList(1));
+        itemsForLeftHand = new HashSet<Integer>(Arrays.asList(6));
+        itemsForRightHand = new HashSet<Integer>(Arrays.asList(7));
+        itemsForLegs = new HashSet<Integer>(Arrays.asList(3));
+        itemsForLeftFoot = new HashSet<Integer>(Arrays.asList(4));
+        itemsForRightFoot = new HashSet<Integer>(Arrays.asList(5));
+        itemsForLeftRing = new HashSet<Integer>(Arrays.asList(0));
+        itemsForRightRing = new HashSet<Integer>(Arrays.asList(0));
+        itemsForNecklace = new HashSet<Integer>(Arrays.asList(0));
     }
 
     public static HashSet<Integer> getItemSetForEquipmentSlot(int slotId){
