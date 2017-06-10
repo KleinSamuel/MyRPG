5fd847cb71bf140a12463fdb55f2a562d773aec6
sam <samuel.klein@online.de>
2017-06-10 16:50:24 +0200

    added functionality for items to be dropped and picked up

--- core/src/com/kleinsamuel/game/GameClass.java
+++ core/src/com/kleinsamuel/game/GameClass.java
@@ -5,6 +5,8 @@ import com.kleinsamuel.game.model.Assets;
 import com.kleinsamuel.game.model.entities.OtherPlayer;
 import com.kleinsamuel.game.model.entities.Player;
 import com.kleinsamuel.game.model.entities.npcs.NPC;
+import com.kleinsamuel.game.model.items.Item;
+import com.kleinsamuel.game.model.maps.MapFactory;
 import com.kleinsamuel.game.screens.PlayScreen;
 import com.kleinsamuel.game.startscreen.StartScreen;
 import com.kleinsamuel.game.util.DebugMessageFactory;
@@ -13,7 +15,10 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 import io.socket.client.IO;
 import io.socket.client.Socket;
@@ -21,7 +26,7 @@ import io.socket.emitter.Emitter;
 
 public class GameClass extends Game {
 
-	private String serverName = "87.160.50.18";
+	private String serverName = "87.160.60.137";
 	private String port = "8081";
 	private IO.Options socketOptions;
 
@@ -43,6 +48,7 @@ public class GameClass extends Game {
 
 	public ConcurrentHashMap<String, OtherPlayer> otherPlayers;
 	public ConcurrentHashMap<Integer, NPC> npcs;
+	public CopyOnWriteArrayList<Item> items;
 
 	@Override
 	public void create() {
@@ -58,6 +64,7 @@ public class GameClass extends Game {
 
 		otherPlayers = new ConcurrentHashMap<String, OtherPlayer>();
 		npcs = new ConcurrentHashMap<Integer, NPC>();
+		items = new CopyOnWriteArrayList<Item>();
 
 		//startScreen = new StartScreen(this);
 		//setScreen(startScreen);
@@ -111,9 +118,6 @@ public class GameClass extends Game {
 				data.put("x", player.content.x);
 				data.put("y", player.content.y);
 				data.put("LEVEL", player.content.LEVEL);
-				data.put("xMove", player.xMove);
-				data.put("yMove", player.yMove);
-				data.put("xPos", player.xPos);
 
 				socket.emit("playerMoved", data);
 
@@ -135,6 +139,25 @@ public class GameClass extends Game {
 
 		} catch (JSONException e){
 			DebugMessageFactory.printErrorMessage("SOCKET:IO ERROR SENDING PLAYER MOVED POINT DATA");
+		}
+	}
+
+	public void playerMovedToAnotherSection(Player p){
+
+		otherPlayers.clear();
+		npcs.clear();
+
+		JSONObject data = new JSONObject();
+		try {
+			data.put("mapIdentifier", MapFactory.parseIdentifierFromDetailToSmall(p.content.mapIdentifier));
+			data.put("x", p.content.x);
+			data.put("y", p.content.y);
+
+			DebugMessageFactory.printInfoMessage("SEND UPDATE FOR SECTION CHANGE");
+
+			socket.emit("playerMovedToAnotherSection", data);
+		} catch (JSONException e){
+			DebugMessageFactory.printErrorMessage("ERROR SENDING PLAYER MOVED TO ANOTHER SECTION UPDATE");
 		}
 	}
 
@@ -197,17 +220,28 @@ public class GameClass extends Game {
 		}
 	}
 
+	public void itemPicked(Item item){
+		JSONObject data = new JSONObject();
+		try {
+			data.put("item_key", item.data.getItem_key());
+			data.put("x", item.data.getX());
+			data.put("y", item.data.getY());
+
+			socket.emit("itemPicked", data);
+		} catch (JSONException e){
+			DebugMessageFactory.printErrorMessage("SOCKET:IO ERROR SENDING ITEM PICKED UPDATE");
+		}
+	}
+
 	public void sendInitialInfo(Player p){
 		JSONObject data = new JSONObject();
 
 		try {
-			data.put("NAME", p.content.NAME);
+			data.put("name", p.content.NAME);
 			data.put("x", p.content.x);
 			data.put("y", p.content.y);
-			data.put("LEVEL", p.content.LEVEL);
-			data.put("xMove", p.xMove);
-			data.put("yMove", p.yMove);
-			data.put("xPos", p.xPos);
+			data.put("mapIdentifier", MapFactory.parseIdentifierFromDetailToSmall(p.content.mapIdentifier));
+			data.put("level", p.content.LEVEL);
 			data.put("currHealth", p.content.CURRENT_HEALTH);
 			data.put("maxHealth", p.content.MAX_HEALTH);
 			data.put("currMana", p.content.CURRENT_MANA);
@@ -252,7 +286,7 @@ public class GameClass extends Game {
 			@Override
 			public void call(Object... args) {
 				CONNECTED = false;
-				//startScreen = new StartScreen(game);
+				startScreen = new StartScreen(game);
 				setScreen(startScreen);
 				playScreen = null;
 				startScreen.signUpScreen = null;
@@ -288,78 +322,88 @@ public class GameClass extends Game {
 					e.printStackTrace();
 				}
 			}
-		}).on("newPlayer", new Emitter.Listener(){
-			@Override
-			public void call(Object... args) {
-				JSONObject data = (JSONObject) args[0];
-				try {
-					String id = data.getString("id");
-					DebugMessageFactory.printInfoMessage("New Player connected: ["+id+"]");
-
-					if(playScreen != null) {
-						playScreen.addOtherPlayer(id);
-					}
-				} catch (JSONException e) {
-					e.printStackTrace();
-				}
-			}
-		}).on("initNewPlayer", new Emitter.Listener(){
+		});
+		socket.on("initNewPlayer", new Emitter.Listener(){
             @Override
             public void call(Object... args) {
                 JSONObject data = (JSONObject) args[0];
                 try {
 
                     String id = data.getString("id");
-                    String name = data.getString("NAME");
+                    String name = data.getString("name");
+					int level = data.getInt("level");
+					String mapIdentifier = MapFactory.parseIdentifierFromSmallToDetail(data.getString("mapIdentifier"));
                     int entityX = data.getInt("x");
                     int entityY = data.getInt("y");
-                    int xMove = data.getInt("xMove");
-                    int yMove = data.getInt("yMove");
-                    int xPos = data.getInt("xPos");
+                    int currentHealth = data.getInt("currHealth");
+                    int maxHealth = data.getInt("maxHealth");
 
                     if(playScreen != null) {
-                        playScreen.updateOtherPlayer(id, name, entityX, entityY, xMove, yMove, xPos);
-                    }
+						if (mapIdentifier.equals(playScreen.player.content.mapIdentifier)) {
+							playScreen.addOtherPlayer(id, name, level, entityX, entityY, currentHealth, maxHealth);
+							DebugMessageFactory.printInfoMessage("INIT NEW PLAYER IN SAME SECTION");
+						}
+					}
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
             }
-        }).on("playerDisconnected", new Emitter.Listener(){
+        });
+		socket.on("playerDisconnected", new Emitter.Listener(){
 			@Override
 			public void call(Object... args) {
 				JSONObject data = (JSONObject) args[0];
 				try {
 					String id = data.getString("id");
 					DebugMessageFactory.printInfoMessage("Player disconnected: ["+id+"]");
+
 					if(playScreen != null) {
 						playScreen.removeOtherPlayer(id);
+						DebugMessageFactory.printInfoMessage("REMOVE DISCONNECTED PLAYER");
 					}
+
+					DebugMessageFactory.printInfoMessage("OtherPlayers Size: "+otherPlayers.size());
+					for(Map.Entry<String, OtherPlayer> entry : otherPlayers.entrySet()){
+						DebugMessageFactory.printInfoMessage("KEY: "+entry.getKey());
+					}
+
 				} catch (JSONException e) {
 					e.printStackTrace();
 				}
 			}
-		}).on("getPlayers", new Emitter.Listener() {
+		});
+		socket.on("getPlayers", new Emitter.Listener() {
 			@Override
 			public void call(Object... args) {
+
 				JSONArray objects = (JSONArray) args[0];
 				try {
 
 					for(int i = 0; i < objects.length(); i++){
 
 						String id = objects.getJSONObject(i).getString("id");
+
+                        if(id.equals(clientID)){
+                            continue;
+                        }
+
 						String name = objects.getJSONObject(i).getString("name");
+                        int level = objects.getJSONObject(i).getInt("level");
+						String mapIdentifier = MapFactory.parseIdentifierFromSmallToDetail(objects.getJSONObject(i).getString("mapIdentifier"));
 						int entityX = objects.getJSONObject(i).getInt("x");
 						int entityY = objects.getJSONObject(i).getInt("y");
-						int xMove = objects.getJSONObject(i).getInt("xMove");
-						int yMove = objects.getJSONObject(i).getInt("yMove");
-						int xPos = objects.getJSONObject(i).getInt("xPos");
+                        int currentHealth = objects.getJSONObject(i).getInt("currHealth");
+                        int maxHealth = objects.getJSONObject(i).getInt("maxHealth");
 
 						if(playScreen != null) {
-							playScreen.addOtherPlayer(id, name, entityX, entityY, xMove, yMove, xPos);
+							if(mapIdentifier.equals(playScreen.player.content.mapIdentifier)) {
+								playScreen.addOtherPlayer(id, name, level, entityX, entityY, currentHealth, maxHealth);
+							}
 						}
 					}
 				} catch(JSONException e){
 					DebugMessageFactory.printErrorMessage("ERROR GETTING OTHER PLAYERS!");
+					e.printStackTrace();
 				}
 			}
 		}).on("playerMoved", new Emitter.Listener() {
@@ -372,12 +416,9 @@ public class GameClass extends Game {
 					String name = data.getString("name");
 					int entityX = data.getInt("x");
 					int entityY = data.getInt("y");
-					int xMove = data.getInt("xMove");
-					int yMove = data.getInt("yMove");
-					int xPos = data.getInt("xPos");
 
 					if(playScreen != null) {
-                        playScreen.updateOtherPlayer(id, name, entityX, entityY, xMove, yMove, xPos);
+                        playScreen.updateOtherPlayer(id, name, entityX, entityY);
 					}
 				} catch(JSONException e){
                     DebugMessageFactory.printErrorMessage("ERROR UPDATING OTHER PLAYERS!");
@@ -400,7 +441,38 @@ public class GameClass extends Game {
                     DebugMessageFactory.printErrorMessage("ERROR GETTING OTHER PLAYERS POINT!");
 				}
 			}
-		}).on("registerNewUserAnswer", new Emitter.Listener() {
+		});
+
+		socket.on("playerMovedToAnotherSection", new Emitter.Listener() {
+			@Override
+			public void call(Object... args) {
+				JSONObject data = (JSONObject) args[0];
+				try {
+
+					String id = data.getString("id");
+					String name = data.getString("name");
+                    int level = data.getInt("level");
+					String mapIdentifier = MapFactory.parseIdentifierFromSmallToDetail(data.getString("mapIdentifier"));
+					int entityX = data.getInt("x");
+					int entityY = data.getInt("y");
+                    int currentHealth = data.getInt("currHealth");
+                    int maxHealth = data.getInt("maxHealth");
+
+					if(playScreen != null){
+						if(mapIdentifier.equals(playScreen.player.content.mapIdentifier)) {
+							playScreen.addOtherPlayer(id, name, level, entityX, entityY, currentHealth, maxHealth);
+						}else{
+							playScreen.removeOtherPlayer(id);
+						}
+					}
+
+				} catch(JSONException e){
+
+				}
+			}
+		});
+
+		socket.on("registerNewUserAnswer", new Emitter.Listener() {
 			@Override
 			public void call(Object... args) {
 				JSONObject data = (JSONObject) args[0];
@@ -428,12 +500,13 @@ public class GameClass extends Game {
 						int level = objects.getJSONObject(i).getInt("level");
 						float x = objects.getJSONObject(i).getInt("x");
 						float y = objects.getJSONObject(i).getInt("y");
+						String mapIdentifier = objects.getJSONObject(i).getString("mapIdentifier");
 						float speed = (float)objects.getJSONObject(i).getDouble("speed");
 						int current_health = objects.getJSONObject(i).getInt("currHealth");
 						int max_health = objects.getJSONObject(i).getInt("maxHealth");
 
                         if(playScreen != null) {
-							playScreen.addNPC(id, npc_key, level, x, y, speed, current_health, max_health);
+							playScreen.addNPC(id, npc_key, level, x, y, mapIdentifier, speed, current_health, max_health);
                         }
 					}
 
@@ -488,12 +561,13 @@ public class GameClass extends Game {
 					int level = data.getInt("level");
 					int x = data.getInt("x");
 					int y = data.getInt("y");
+					String mapIdentifier = data.getString("mapIdentifier");
 					double speed = data.getDouble("speed");
 					int current_health = data.getInt("current_health");
 					int max_health = data.getInt("max_health");
 
 					if(playScreen != null) {
-						playScreen.addNPC(id, npc_key, level, x, y, (float)speed, current_health, max_health);
+						playScreen.addNPC(id, npc_key, level, x, y, mapIdentifier, (float)speed, current_health, max_health);
 					}
 
 				} catch(JSONException e){
@@ -511,7 +585,7 @@ public class GameClass extends Game {
 					int amount = data.getInt("amount");
 
 					if(playScreen != null) {
-						playScreen.damageNpc(toId, amount);
+						playScreen.damageNpc(fromId, toId, amount, false);
 					}
 
 				} catch(JSONException e){
@@ -549,6 +623,44 @@ public class GameClass extends Game {
 					}
 				} catch(JSONException e){
 					DebugMessageFactory.printErrorMessage("ERROR AT DAMAGE TO PLAYER");
+				}
+			}
+		});
+		socket.on("itemDropped", new Emitter.Listener() {
+			@Override
+			public void call(Object... args) {
+				JSONObject data = (JSONObject) args[0];
+				try {
+					int item_key = data.getInt("item_key");
+					int x = data.getInt("x");
+					int y = data.getInt("y");
+					int amount = data.getInt("amount");
+					String ownerId = data.getString("ownerId");
+
+					if(playScreen != null){
+						playScreen.addItem(item_key, x, y, amount, ownerId);
+					}
+
+				} catch(JSONException e){
+					DebugMessageFactory.printErrorMessage("ERROR GETTING ITEM DROP");
+				}
+			}
+		});
+		socket.on("itemPicked", new Emitter.Listener() {
+			@Override
+			public void call(Object... args) {
+				JSONObject data = (JSONObject) args[0];
+				try {
+					int item_key = data.getInt("item_key");
+					int x = data.getInt("x");
+					int y = data.getInt("y");
+
+					if(playScreen != null){
+						playScreen.removeItem(item_key, x, y);
+					}
+
+				} catch(JSONException e){
+					DebugMessageFactory.printErrorMessage("ERROR GETTING ITEM DROP");
 				}
 			}
 		});
