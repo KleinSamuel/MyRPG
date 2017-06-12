c620fcefef140abfb6af20e48d0227eb738d543f
sam <samuel.klein@online.de>
2017-06-12 17:29:07 +0200

    played around with sound manager but changed nothing important

--- android/src/com/kleinsamuel/game/AndroidLauncher.java
+++ android/src/com/kleinsamuel/game/AndroidLauncher.java
@@ -1,9 +1,13 @@
 package com.kleinsamuel.game;
 
+import android.media.AudioAttributes;
+import android.media.AudioManager;
+import android.media.SoundPool;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
 
+import com.badlogic.gdx.assets.loaders.SoundLoader;
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 
@@ -12,12 +16,6 @@ public class AndroidLauncher extends AndroidApplication {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-
-		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		config.useImmersiveMode = true;
-		config.hideStatusBar = true;
-		config.useWakelock = true;
-		initialize(new GameClass(), config);
 
 		View decorView = getWindow().getDecorView();
 		int uiOptions = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
@@ -28,6 +26,20 @@ public class AndroidLauncher extends AndroidApplication {
 				| View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
 		decorView.setSystemUiVisibility(uiOptions);
 
+		AudioAttributes attr = new AudioAttributes.Builder()
+				.setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+				.setFlags(AudioAttributes.FLAG_AUDIBILITY_ENFORCED)
+				.setUsage(AudioAttributes.USAGE_GAME)
+				.build();
+
+		SoundPool soundPool = new SoundPool.Builder().setAudioAttributes(attr).build();
+
 		getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		config.useImmersiveMode = true;
+		config.hideStatusBar = true;
+		config.useWakelock = true;
+		initialize(new GameClass(), config);
 	}
 }
