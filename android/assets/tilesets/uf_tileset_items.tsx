3b8269b46393c5054ace55a00ec57a28b1465811
sam <samuel.klein@online.de>
2017-06-13 02:20:09 +0200

    added tilesets

---
+++ android/assets/tilesets/uf_tileset_items.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="uf_items" tilewidth="24" tileheight="24" tilecount="1936" columns="44">
+ <image source="uf_items.png" width="1056" height="1056"/>
+</tileset>
