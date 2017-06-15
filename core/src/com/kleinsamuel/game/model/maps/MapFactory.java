f40d8769fbe115f3b6d5bfa9daa3a0abf9f356ad
sam <samuel.klein@online.de>
2017-06-15 19:51:43 +0200

    added interactive tiles to map and hardcoded item spawns

--- core/src/com/kleinsamuel/game/model/maps/MapFactory.java
+++ core/src/com/kleinsamuel/game/model/maps/MapFactory.java
@@ -1,6 +1,12 @@
 package com.kleinsamuel.game.model.maps;
 
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.math.Vector3;
+import com.kleinsamuel.game.model.items.Item;
+import com.kleinsamuel.game.model.items.ItemData;
+import com.kleinsamuel.game.model.items.ItemFactory;
+import com.kleinsamuel.game.model.maps.interactive.MessageInteraction;
+import com.kleinsamuel.game.util.Utils;
 
 import java.util.ArrayList;
 
@@ -18,18 +24,41 @@ public class MapFactory {
     public static String START_LAWN = "maps/start_lawn/uf_start_lawn.tmx";
 
     public static MapSection getMapSectionForIdentifier(String mapSectionIdentifier){
-        return new MapSection(mapSectionIdentifier, mapLoader.load(mapSectionIdentifier), getBeamableTilesForIdentfier(mapSectionIdentifier));
+        return new MapSection(mapSectionIdentifier, mapLoader.load(mapSectionIdentifier), getBeamableTilesForIdentfier(mapSectionIdentifier), getInteractiveTilesForIdentifier(mapSectionIdentifier), getItemsForIdentifier(mapSectionIdentifier));
     }
 
     public static ArrayList<BeamableTilePair> getBeamableTilesForIdentfier(String mapSectionIdentifier){
         ArrayList<BeamableTilePair> outputList = new ArrayList<BeamableTilePair>();
         if(mapSectionIdentifier.equals(START_HOUSE)){
-            outputList.add(new BeamableTilePair(START_HOUSE, 12, 22, START_HOUSE, 50, 21));
-            outputList.add(new BeamableTilePair(START_HOUSE, 50, 21, START_HOUSE, 12, 22));
-            outputList.add(new BeamableTilePair(START_HOUSE, 48, 8, START_LAWN, 11, 20));
+            outputList.add(new BeamableTilePair(START_HOUSE, 11, 16, START_HOUSE, 32, 15));
+            outputList.add(new BeamableTilePair(START_HOUSE, 32, 15, START_HOUSE, 11, 16));
+            outputList.add(new BeamableTilePair(START_HOUSE, 30, 3, START_LAWN, 21, 17));
+            outputList.add(new BeamableTilePair(START_HOUSE, 31, 3, START_LAWN, 21, 17));
         }else if(mapSectionIdentifier.equals(START_LAWN)){
             //outputList.add(new BeamableTile(48, 8, 11, 21));
         }
+        return outputList;
+    }
+
+    public static ArrayList<InteractiveTile> getInteractiveTilesForIdentifier(String mapSectionIdentifier){
+        ArrayList<InteractiveTile> outputList = new ArrayList<InteractiveTile>();
+        if(mapSectionIdentifier.equals(START_HOUSE)){
+            outputList.add(new InteractiveTile(5, 20, new MessageInteraction(1000, 0, 1)));
+        }else if(mapSectionIdentifier.equals(START_LAWN)){
+
+        }
+        return outputList;
+    }
+
+    public static ArrayList<Item> getItemsForIdentifier(String mapSectionIdentifier){
+        ArrayList<Item> outputList = new ArrayList<Item>();
+
+        if(mapSectionIdentifier.equals(START_HOUSE)){
+            outputList.add(new Item(new ItemData(1000, 1000, 5* Utils.TILEWIDTH, 5*Utils.TILEHEIGHT, 1, "")));
+        }else if(mapSectionIdentifier.equals(START_LAWN)){
+
+        }
+
         return outputList;
     }
 
--- core/src/com/kleinsamuel/game/model/maps/MapSection.java
+++ core/src/com/kleinsamuel/game/model/maps/MapSection.java
@@ -1,11 +1,14 @@
 package com.kleinsamuel.game.model.maps;
 
+import com.badlogic.gdx.maps.MapLayer;
+import com.badlogic.gdx.maps.MapLayers;
 import com.badlogic.gdx.maps.MapObject;
 import com.badlogic.gdx.maps.MapProperties;
 import com.badlogic.gdx.maps.objects.RectangleMapObject;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.math.Rectangle;
+import com.kleinsamuel.game.model.items.Item;
 import com.kleinsamuel.game.util.DebugMessageFactory;
 
 import java.util.ArrayList;
@@ -31,12 +34,16 @@ public class MapSection {
     public ArrayList<Rectangle> walkableRectangles;
 
     public ArrayList<BeamableTilePair> beamableTiles;
+    public ArrayList<InteractiveTile> interactiveTiles;
+    public ArrayList<Item> items;
 
-    public MapSection(String identifier, TiledMap map, ArrayList<BeamableTilePair> beamableTiles){
+    public MapSection(String identifier, TiledMap map, ArrayList<BeamableTilePair> beamableTiles, ArrayList<InteractiveTile> interactiveTiles, ArrayList<Item> items){
         this.identifier = identifier;
         this.map = map;
         this.mapRenderer = new MyOrthogonalTileMapRenderer(map);
         this.beamableTiles = beamableTiles;
+        this.interactiveTiles = interactiveTiles;
+        this.items = items;
 
         properties = map.getProperties();
         mapWidth = properties.get("width", Integer.class);
@@ -44,7 +51,7 @@ public class MapSection {
 
         this.walkableRectangles = new ArrayList<Rectangle>();
 
-        for(MapObject mo : map.getLayers().get("objects").getObjects()){
+        for(MapObject mo : map.getLayers().get("objects_players").getObjects()){
             if(mo instanceof RectangleMapObject){
                 Rectangle re = ((RectangleMapObject)mo).getRectangle();
                 walkableRectangles.add(re);
@@ -52,10 +59,13 @@ public class MapSection {
         }
 
         DebugMessageFactory.printInfoMessage("RECTANGLES NPC:");
-        for(MapObject mo : map.getLayers().get("objects_npc").getObjects()){
-            if(mo instanceof RectangleMapObject){
-                Rectangle re = ((RectangleMapObject)mo).getRectangle();
-                DebugMessageFactory.printInfoMessage(re.toString());
+        MapLayer npcLayer = map.getLayers().get("objects_npcs");
+        if(npcLayer != null) {
+            for (MapObject mo : map.getLayers().get("objects_npcs").getObjects()) {
+                if (mo instanceof RectangleMapObject) {
+                    Rectangle re = ((RectangleMapObject) mo).getRectangle();
+                    DebugMessageFactory.printInfoMessage(re.toString());
+                }
             }
         }
 
